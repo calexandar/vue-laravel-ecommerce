9ac4f85b8a7957416e9e6611c667a116d54f6d45
calexandar <alexandercvet243@gmail.com>
2023-02-02 11:23:00 +0100

    backend product aded

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, HasSlug;
+
+    protected $fillable = [
+                            'title',
+                            'description',
+                            'price',
+                            'image',
+                            'published',
+                            'image_mime',
+                            'image_size',
+                            'created_by',
+                            'updated_by'
+                         ];
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+
+    public function getRouteName()
+    {
+        return 'slug';
+    }
 }
