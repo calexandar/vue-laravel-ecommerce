6b26253a56230f451eca84b8ce566e7e64d6c23a
calexandar <alexandercvet243@gmail.com>
2023-02-07 12:45:43 +0100

    aded product table sort and pagination

--- backend/src/views/Products/ProductTable.vue
+++ backend/src/views/Products/ProductTable.vue
@@ -3,7 +3,7 @@
         <div class="flex justify-between border-b-2 pb-3">
             <div class="flex items-center">
                 <span class="whitespace-nowrap mr-3">Per Page</span>
-                <select 
+                <select v-model="perPage" @change="getProducts(null)"
                     class="appearance-none relative block w-24 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 >
                     <option value="5">5</option>
@@ -12,10 +12,10 @@
                     <option value="50">50</option>
                     <option value="100">100</option>
                 </select>
-                <span class="ml-3">Found [total] products</span>
+                <span class="ml-3">Found {{ products.total }} products</span>
             </div>
             <div>
-                <input 
+                <input v-model="search" @change="getProducts(null)"
                     class="appearance-none relative block w-48 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="Type to Search products"
                     >   
@@ -25,7 +25,21 @@
         <table class="table-auto w-full">
             <thead>
                 <tr>
-
+                    <TableHeaderCell field="id" :sort-field="sortField" :sort-direction="sortDirection" @click="sortProducts('id')">
+                        ID
+                    </TableHeaderCell>
+                    <TableHeaderCell field="image" :sort-field="sortField" :sort-direction="sortDirection">
+                        Image
+                    </TableHeaderCell>
+                    <TableHeaderCell field="title" :sort-field="sortField" :sort-direction="sortDirection" @click="sortProducts('title')">
+                        Title
+                    </TableHeaderCell>
+                    <TableHeaderCell field="price" :sort-field="sortField" :sort-direction="sortDirection" @click="sortProducts('price')">
+                        Price
+                    </TableHeaderCell>
+                    <TableHeaderCell field="updated_at" :sort-field="sortField" :sort-direction="sortDirection" @click="sortProducts('updated_at')">
+                        Last Updated At
+                    </TableHeaderCell>
                 </tr>
             </thead>
             <tbody v-if="products.loading || !products.data.length">
@@ -114,29 +128,93 @@
             </tbody>
         </table>
     </div>
+
+    <div v-if="!products.loading" class="flex justify-between items-center mt-5">
+      <div v-if="products.data.length">
+        Showing from {{ products.from }} to {{ products.to }}
+      </div>
+      <nav
+        v-if="products.total > products.limit"
+        class="relative z-0 inline-flex justify-center rounded-md shadow-sm -space-x-px"
+        aria-label="Pagination"
+      >
+        <!-- Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" -->
+        <a
+          v-for="(link, i) of products.links"
+          :key="i"
+          :disabled="!link.url"
+          href="#"
+          @click="getForPage($event, link)"
+          aria-current="page"
+          class="relative inline-flex items-center px-4 py-2 border text-sm font-medium whitespace-nowrap"
+          :class="[
+              link.active
+                ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600'
+                : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50',
+              i === 0 ? 'rounded-l-md' : '',
+              i === products.links.length - 1 ? 'rounded-r-md' : '',
+              !link.url ? ' bg-gray-100 text-gray-700': ''
+            ]"
+          v-html="link.label"
+        >
+        </a>
+      </nav>
+    </div>
+
 </template>
 
 <script setup>
 import { computed, onMounted, ref } from 'vue';
 import store from '../../store';
 import Spinner from '../../components/core/Spinner.vue';
+import TableHeaderCell from '../../components/core/Table/TableHeaderCell.vue';
+import {PRODUCTS_PER_PAGE} from "../../constants";
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue';
 import { EllipsisVerticalIcon, PencilSquareIcon, TrashIcon } from '@heroicons/vue/24/outline';
 
-
+const perPage = ref(PRODUCTS_PER_PAGE);
+const search = ref('');
+const sortField = ref('updated_at');
+const sortDirection = ref('desc');
 const products = computed(()=> store.state.products);
 const showProductModal = ref(false);
 const emit = defineEmits(['clickEdit'])
 
 onMounted(()=>{
     getProducts();
-//needs more fields for search
 })
+
+function getForPage(ev, link) {
+  ev.preventDefault();
+  if (!link.url || link.active) {
+    return;
+  }
+  getProducts(link.url)
+}
 
 function getProducts(url=null) {
     store.dispatch("getProducts", {
         url,
+        search: search.value,
+        per_page: perPage.value,
+        sort_field: sortField.value,
+        sort_direction: sortDirection.value 
     });
+}
+
+function sortProducts(field) {
+    if(field === sortField.value) {
+        if(sortDirection.value === 'desc'){
+            sortDirection.value = 'asc'
+        }else {
+            sortDirection.value = 'desc'
+        }
+    }else {
+        sortField.value = field;
+        sortDirection.value = 'asc'
+    }
+
+    getProducts()
 }
 
 function showAddNewModal(){
